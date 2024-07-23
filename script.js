958463971390472042fa81ee3defabb0c81fce1b
harshitanchan <harshitanchan647@gmail.com>
2024-07-23 20:55:45 +0530

    node

---
+++ script.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+
+// fs.rename("hey.txt", "hello.txt", function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("done");
+//     }
+// })
+
+// fs.copyFile("hello.txt", "./copy2/copy.txt", function(err){
+//     if(err) {
+//         console.log(err.message);
+//     }else{
+//         console.log("done");
+//     }
+// })
+
+
+// fs.unlink("hello.txt", function(err){
+//     if(err) {
+//         console.log(err.message);
+//     }else console.log("done");
+// })
+
+
